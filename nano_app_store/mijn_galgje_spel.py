40792fd14fe54b857bbccaa9d777c04691c37848
seymaceren <s.ceren2003@gmail.com>
2024-09-28 22:06:43 +0200

    Werk aan galgje spel
    test

--- nano_app_store/mijn_galgje_spel.py
+++ nano_app_store/mijn_galgje_spel.py
@@ -5,11 +5,12 @@
 # Van elke gebruiker wordt  de score met de datum bijgehouden in een scorebestand
 import json
 import os
+import random
 from json import JSONEncoder
 
 woorden = """{"makkelijk": ["bal", "appel", "klok","vogel","zon","maan","rups","kalf","haas","kip"], 
                        "gemiddeld": ["bankstel", "laptoptas", "computer","geniaal", "vloerkleed","examen","schoolbus","regenlaarzen","capuchon","teamleider"],
-                       "moeijlijk": ["boodschappenlijstje", "paraferen", "geavanceerd","raamkozijn","gegeneraliseerd","eloquent","traditiegetrouw","belangeloos","adequaat"] }"""
+                       "moeilijk": ["boodschappenlijstje", "paraferen", "geavanceerd","raamkozijn","gegeneraliseerd","eloquent","traditiegetrouw","belangeloos","adequaat"] }"""
 
 def teken_galg(aantal_fouten):
     hangman = [""" 
@@ -71,6 +72,31 @@ def speel_galg():
         naam = input("Wat is uw naam? ")
         moeilijkheidsgraad = input("Welke moeilijkheidsgraad? Makkelijk, gemiddeld of moeilijk? ")
         woorden_op_moeilijkheidsgraad = json_data[moeilijkheidsgraad]
-        print(woorden_op_moeilijkheidsgraad)
+        #print(woorden_op_moeilijkheidsgraad)
+        woord = random.choice(woorden_op_moeilijkheidsgraad)
+        print(woord)
+        geraden_letters = []
+        is_running = True
+
+        while is_running:
+            letters_nog_te_raden = []
+            woord_of_letter = input(f"{naam} Raad het woord of letter? ")
+
+            if len(woord_of_letter) > 1 and woord_of_letter == woord:
+                print(f"{naam}, gefeliciteerd! Je hebt het woord geraden.")
+                is_running = False
+            else:
+                if len(woord_of_letter) and woord_of_letter in woord:
+                    if woord_of_letter not in geraden_letters:
+                        geraden_letters.append(woord_of_letter)
+                    else:
+                        print("Deze letter heb je al ingevoerd.")
+
+                for letter in woord:
+                    if letter not in geraden_letters:
+                        letters_nog_te_raden.append('_')
+                    else:
+                        letters_nog_te_raden.append(letter)
+                print(letters_nog_te_raden)
 
 speel_galg()
