caf1da44592db25713fb9120fd64c08e451a8d39
ceren <s.ceren2003@gmail.com>
2024-10-06 17:34:20 +0200

    Het weer app

---
+++ hetweer.py
@@ -0,0 +1,18 @@
+import python_weather
+import asyncio
+
+
+# We maken hier gebruik van python_weater API om het weer van een bepaalde stad op te vragen
+async def geef_temparatuur() -> None:
+    stad = input("Welke stad: ")
+    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+        # Vraag van een stad het weer
+        weather = await client.get(stad)
+
+        # geeft de temparatuur terug in graden celcius
+        print(f"{int(round((weather.temperature - 32) * 5/9, 0))}°C")
+
+def run():
+    asyncio.run(geef_temparatuur())
+if __name__ == '__main__':
+    run()
--- nano_app_store.py
+++ nano_app_store.py
@@ -6,6 +6,7 @@ def main():
     print("Kies een spel om te spelen.")
     print("1. Nummer Raad Spel")
     print("2. Galg spel")
+    print("3. Het weer")
     keuze = int(input())
 
     if keuze == 1:
@@ -13,6 +14,9 @@ def main():
     elif keuze == 2:
         import mijn_galgje_spel
         mijn_galgje_spel.speel_galg()
+    elif keuze == 3:
+        import hetweer
+        hetweer.run()
 
 
 if __name__ == '__main__':
